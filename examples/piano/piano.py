3ca00ffa64dc0f284bb3a9f2873eb73389a35498
Vinay <nk.vinay@zohomail.in>
2023-11-10 19:39:22 +0530

    Cleanup piano example and add more documentation

--- examples/piano/piano.py
+++ examples/piano/piano.py
@@ -1,7 +1,17 @@
 """
-This is a simple keyboard based synthesizer. It uses the getch library to get
-the keypresses, koelsynth to generate the audio, and pyaudio to play the
-generated audio.
+A simple keyboard based piano app.
+
+It works as follows:
+    1. Use getch provided by py-getch library to get keyboard input
+    2. Use koelsynth to produce FM synth audio
+    3. Use pyaudio to play the produced audio
+
+The keyboard input is handled in the main thread. Audio processing is handled
+in a separate thread.
+
+Note: PyAudio may print some logs during the startup. This can be ignored.
+
+See get_synth_params() to adjust the audio.
 """
 
 import threading
@@ -24,8 +34,10 @@ key_str_to_int: dict[str, int] = {}
 # Mapping of keyboard keys to piano keys
 key_remap: dict[str, str] = {}
 
-# Supported keys
+# Supported keys (from your keyboard)
 input_keys = "q w e r t y u i o p"
+# Mapped piano keys for the above chars
+piano_keys = "1c 1d 1e 1g 1a 2c 2d 2e 2g 2a"
 
 
 def build_key_str_map() -> None:
@@ -42,10 +54,10 @@ def build_key_remap() -> None:
     """
     Mapping keyboard keys to piano keys
     """
-    kb_keys = input_keys.split()
-    # pentatonic scale (b and f are omitted)
-    piano_keys = "1c 1d 1e 1g 1a 2c 2d 2e 2g 2a".split()
-    for kb_key, piano_key in zip(kb_keys, piano_keys):
+    source_keys = input_keys.split()
+    target_keys = piano_keys.split()
+    assert len(source_keys) == len(target_keys)
+    for kb_key, piano_key in zip(source_keys, target_keys):
         key_remap[kb_key] = piano_key
 
 
@@ -100,10 +112,14 @@ def handle_sequencer_audio(
     stream.close()
 
 
-def handle_key_press(
-    sequencer: koelsynth.Sequencer,
-    lock: threading.Lock,
-) -> None:
+def get_synth_params() -> (
+    tuple[koelsynth.FmSynthModParams, koelsynth.AdsrParams, koelsynth.AdsrParams]
+):
+    """
+    Return FM synthesis parameters.
+
+    This is the function to play around with the sound texture.
+    """
     # FM Modulation parameters
     synth_params = koelsynth.FmSynthModParams([2, 5, 9, 13], [1, 2, 1, 1])
     # Modulation envelope
@@ -125,39 +141,60 @@ def handle_key_press(
         slevel2=0.1,
     )
     assert wav_env.get_size() == mod_env.get_size()
-    # Setup curses interface (blank)
-    time.sleep(2)
+    return synth_params, mod_env, wav_env
+
+
+def handle_key_press(
+    sequencer: koelsynth.Sequencer,
+    lock: threading.Lock,
+) -> None:
+    """
+    Setup synthesis parameters and handle key-press.
+    """
+    synth_params, mod_env, wav_env = get_synth_params()
+    # Wait for pyaudio prints. This is a dirty way to manage this.
+    # TODO: move to better synchronization!
+    time.sleep(3)
     print("\n" + "--" * 40)
     print("Once started, use ` or ? to quit. Ctrl+C may not work!!")
     print("Supported input keys:", input_keys)
     input("Press enter to start:")
     print(flush=True)
     while True:
+        # Get input from keyboard
         ch = getch()
+        # Ctrl+C does not seem to work. We need a way to quit nicely.
         if ch == "`" or ch == "?":
             break
         if ch not in key_remap:
             print(f"\nKey {ch} not recognized! Use ` or ? to exit.")
             continue
+        # Remap input key to corresponding piano key
         piano_key = key_remap[ch.lower()]
+        # This handles the frequency of the tone that will be played
         phase_per_sample = get_phase_per_sample(piano_key)
         with lock:
+            # Add a new FM synth event
             sequencer.add_fmsynth(synth_params, mod_env, wav_env, phase_per_sample)
 
 
 def start_audio_processing_thread(
     sequencer: koelsynth.Sequencer, lock: threading.Lock
 ) -> None:
+    """
+    Setup the thread to handle sequencer processing and audio output
+    """
     thread = threading.Thread(target=handle_sequencer_audio, args=(sequencer, lock))
     thread.daemon = True
     thread.start()
 
 
 def main():
+    """
+    The main function of this script.
+    """
     build_key_str_map()
     build_key_remap()
-    print(key_str_to_int)
-    print(key_remap)
     lock = threading.Lock()
     sequencer = koelsynth.Sequencer(frame_size, gain)
     start_audio_processing_thread(sequencer, lock)
